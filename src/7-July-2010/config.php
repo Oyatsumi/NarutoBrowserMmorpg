ad837ed7afb1d04ffe044181aece93d1d0efe651
Érick Oliveira Rodrigues <erickr@id.uff.br>
2018-09-27 15:27:03 -0300

    A
    A

--- src/7-July-2010/config.php
+++ src/7-July-2010/config.php
@@ -3,7 +3,7 @@
 $dbsettings = Array(
         "server"        => "localhost",     // MySQL server name. (Default: localhost)
         "user"          => "50126",              // MySQL username.
-        "pass"          => "220292eu",              // MySQL password.
+        "pass"          => "123",              // MySQL password.
         "name"          => "50126db3",              // MySQL database name.
         "prefix"        => "dk",            // Prefix for table names. (Default: dk)
         "secretword"    => "erickoya");             // Secret word used when hashing information for cookies.
